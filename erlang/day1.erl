95e4873e9bcab009bf388dd1d3337ea3e75b0d50
Julian Simioni <julian.simioni@gmail.com>
2014-11-28 16:02:52 +0100

    [erlang] Add success_printer function from day1

--- erlang/day1.erl
+++ erlang/day1.erl
@@ -21,9 +21,19 @@ counter(Upto) ->
 	counter(Upto - 1),
 	io:fwrite("~p\n", [Upto]).
 
+success_printer(success) ->
+	io:fwrite("success\n");
+success_printer({error, Message}) ->
+	io:fwrite("error: ~p\n", [Message]).
+
 start() ->
     io:fwrite("Erlang day 1\n"),
 	io:fwrite("~p\n", [wordcount("hello world!\n")]),
 	io:fwrite("~p\n", [wordcountr("hello world!\n")]),
 	io:fwrite("\n\n"),
-	counter(10).
+
+	counter(10),
+	io:fwrite("\n\n"),
+
+	success_printer(success),
+	success_printer({error, "Sorry, something went wrong."}).
