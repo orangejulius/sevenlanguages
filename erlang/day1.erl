6eb28c9b6fdc95d95b5a2838156b50f862cf8182
Julian Simioni <julian.simioni@gmail.com>
2014-11-28 16:00:37 +0100

    [erlang] Add recursive counter function

--- erlang/day1.erl
+++ erlang/day1.erl
@@ -14,7 +14,16 @@ wordcountr_internal([]) -> 1;
 wordcountr_internal([_|T]) ->
 	wordcountr_internal(T) + 1.
 
+counter(0) ->
+	done;
+
+counter(Upto) ->
+	counter(Upto - 1),
+	io:fwrite("~p\n", [Upto]).
+
 start() ->
     io:fwrite("Erlang day 1\n"),
 	io:fwrite("~p\n", [wordcount("hello world!\n")]),
-	io:fwrite("~p\n", [wordcountr("hello world!\n")]).
+	io:fwrite("~p\n", [wordcountr("hello world!\n")]),
+	io:fwrite("\n\n"),
+	counter(10).
